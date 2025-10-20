8242cf94842d517637431e23284eeec05d5eb69e
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-20 15:17:22 +0300

    les deux files soub sont modifier

--- soub.py
+++ soub.py
@@ -1,2 +1,4 @@
 import pandas as pd 
 import numpy as np 
+
+print ( " TEST TEST  1 2")
--- soub1.py
+++ soub1.py
@@ -1,1 +1,3 @@
-print ( " hello souber omar ")
+# print ( " hello souber omar ")
+print ( " test 1 2 ")
+print (" la vie est belle ")
