8e521ab825cfde568f07b19a5419a346ed3fd642
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-23 09:44:47 +0300

    ajout somme number

---
+++ test.py
@@ -0,0 +1,18 @@
+import pandas as pd 
+
+def number(a, b):
+    
+    return a + b
+
+def main():
+    
+    a = 5
+    b = 3
+
+    resultat = number(a, b)
+    
+    print(f"La somme de {a} et {b} est : {resultat}")
+
+
+if __name__ == "__main__":
+    main()
