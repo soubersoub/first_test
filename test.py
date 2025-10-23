246453f5ccc5d79129c707e73234f192dfa226f3
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-23 10:09:24 +0300

    ajout d'une fonction soustraction

--- test.py
+++ test.py
@@ -4,14 +4,32 @@ def number(a, b):
     
     return a + b
 
+
+def soustraction(c, d):
+    if c > d:
+        resultat = c - d
+        return resultat
+    else:
+        print(f"{d} est supérieur ou égal à {c}, le résultat sera négatif ou nul.")
+        resultat = c - d
+        return resultat
+
+
 def main():
     
     a = 5
     b = 3
 
+    c = 1
+    d = 4
+
+    result = soustraction(c,d)
+
     resultat = number(a, b)
     
     print(f"La somme de {a} et {b} est : {resultat}")
+    print (result)
+
 
 
 if __name__ == "__main__":
