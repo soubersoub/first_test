704f3184a1b98af29536349dc4d6522d1784df6c
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-23 15:08:52 +0300

    test 2 ajout soustraction

--- test2.py
+++ test2.py
@@ -1,1 +1,10 @@
 import numpy as np 
+
+def soustraction(c, d):
+    if c > d:
+        resultat = c - d
+        return resultat
+    else:
+        print(f"{d} est supérieur ou égal à {c}, le résultat sera négatif ou nul.")
+        resultat = c - d
+        return resultat
