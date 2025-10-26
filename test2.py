11d5606be77f308c8d07d83629b755366b36d24a
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-26 12:03:34 +0300

    tewt

--- test2.py
+++ test2.py
@@ -1,12 +1,12 @@
 def number(a, b):
-    
-    return a + b
+    v= a+ b
+    return v 
 
 def main():
-
+    a = 5
+    b = 3
     resultat = number(a, b)
-
-
+    print(resultat)
 
 
 if __name__ == "__main__":
