e94b1622dec09994aafe9eb2c5ebf08a327c737e
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-26 15:02:47 +0300

    calcule age add

--- new.py
+++ new.py
@@ -2,12 +2,20 @@ def number(a, b):
     v= a+ b
     return v 
 
+def calcul_age(age):
+    calc = 2025 - age
+    return calc 
 
 def main():
     a = 5
     b = 3
     resultat = number(a, b)
     print(resultat)
+
+    ages = 1996
+
+    test_age = calcul_age(ages) 
+    print(f"Votre age est {test_age}")
 
 
 if __name__ == "__main__":
