4cd8780d4e0ad668f7045cb723d9fe9aab04a110
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-26 12:50:21 +0300

    SOUBER ajout name'

--- test2.py
+++ test2.py
@@ -2,11 +2,20 @@ def number(a, b):
     v= a+ b
     return v 
 
+def prenom (nom):
+    
+    v = nom 
+    return v  
+
 def main():
     a = 5
     b = 3
     resultat = number(a, b)
     print(resultat)
+
+    noms= "SOUBER "
+    name = prenom(noms)
+    print(name)
 
 
 if __name__ == "__main__":
