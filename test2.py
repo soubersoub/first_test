95ebd3354a56dd2ade1553e3371d9b066b76a84d
Souber Omar <souber.omar.ahmed@gmail.com>
2025-10-26 12:54:30 +0300

    soustraction de deux nombre

--- test2.py
+++ test2.py
@@ -2,11 +2,22 @@ def number(a, b):
     v= a+ b
     return v 
 
+
+def soustraction (c,d):
+    f = c-d 
+    return f 
+
 def main():
     a = 5
     b = 3
     resultat = number(a, b)
     print(resultat)
+
+    l =10
+    m = 5
+    mm = soustraction(l,m)
+    print(mm)
+
 
 
 if __name__ == "__main__":
